4aea312739a645a7ef180e2fc52a245ce3b51aa2
eluda315 <michelle3hg@gmail.com>
2025-09-23 15:06:33 +0900

    feat: finish Poly_Arr1

--- L03/Poly_Arr1/HonGong.c
+++ L03/Poly_Arr1/HonGong.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#define MAX(a,b) (((a)>(b)?(a):(b)))
+#define MAX_DEGREE 101
+
+typedef struct tagPolynomial {
+	int degree; // polynomial order
+	float coef[MAX_DEGREE]; // polynomial coefficients
+} polynomial;
+
+// C=A+B (A,B는 다항식)
+polynomial poly_add1(polynomial A, polynomial B)
+{
+	polynomial C; // result
+	int Apos = 0, Bpos = 0, Cpos = 0; // Array index variables
+	int degree_a = A.degree;
+	int degree_b = B.degree;
+	C.degree = MAX(A.degree, B.degree); // result polynomial order
+
+	while (Apos <= A.degree && Bpos <= B.degree) {
+		if (degree_a > degree_b) {
+			C.coef[Cpos++] = A.coef[Apos++];
+			degree_a--;
+		} 
+		else if (degree_a == degree_b) {
+			C.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
+			degree_a--;
+			degree_b--;
+		}
+		else {
+			C.coef[Cpos++] = B.coef[Bpos++];
+			degree_b--;
+		}
+		
+	}
+
+	return C;
+}
 
 int main(vioid)
 {
+	polynomial a = { 5,{3,6,0,0,0,10} };
+	polynomial b = { 4,{7,0,5,0,1} };
+	polynomial c;
+	c = poly_add1(a, b);
 
+	for (int i = 0; i <= c.degree; i++) {
+		printf("%.0f ", c.coef[i]);
+	}
+
+	return 0;
 }
